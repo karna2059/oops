cc56649cfdef9bf8a10d2e3934d5959483527949
kkar2 <kkar2@LUFFY>
2026-05-11 12:47:31 +0530

    made changes

---
+++ oops/methodswithparametersandwithreturnvalue.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace oops
+//{
+//    class myclass
+//    {
+//        public int sum(int a, int b)
+//        {
+//            int c;
+//            c = a + b;
+//            return c;
+//        }
+//        internal class methodswithparametersandwithreturnvalue
+//        {
+//            static void Main(string[] args)
+//            {
+//                myclass myclass = new myclass();
+//                int result = myclass.sum(40, 20);
+//                Console.WriteLine("the sum of a+b is:" + result);
+//            }
+//        }
+//    }
+//}
--- oops/practice.cs
+++ oops/practice.cs
@@ -1,29 +1,42 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace oops
-//{
-//    class myclass
-//    {
-//        public void Greet()
-//        {
-//            Console.WriteLine("karna");
-//        }
-//        public void Greet2(string x)
-//        {
-//            Console.WriteLine(x);
-//        }
-//    }
-//    internal class practice
-//    {
-//        static void Main(string[] args)
-//        {
-//            myclass myclass = new myclass();
-//            myclass.Greet();
-//            myclass.Greet2("Hello" + " karna");
-//        }
-//    }
-//}
+namespace oops
+{
+    class myclass
+    {
+        public void Greet()
+        {
+            Console.WriteLine("karna");
+        }
+        public void Greet2(string x)
+        {
+            Console.WriteLine(x);
+        }
+        public string name()
+        {
+            string a = "karna";
+            return a;
+        }
+        public string name2(string b)
+        {
+            return b;
+        }
+    }
+    internal class practice
+    {
+        static void Main(string[] args)
+        {
+            myclass myclass = new myclass();
+            myclass.Greet();
+            myclass.Greet2("Hello" + " karna");
+            string name = myclass.name();
+            Console.WriteLine("your name is :" + name);
+            string name2 = myclass.name2("karna");
+            Console.WriteLine("your name is :" + name2);
+        }
+    }
+}
