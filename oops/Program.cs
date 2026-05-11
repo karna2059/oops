c2b3455e2eb4c1ce06626510bc75958161613d60
kkar2 <kkar2@LUFFY>
2026-05-11 11:52:52 +0530

    Add project files.

---
+++ oops/Program.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace oops
+//{
+//    class counter
+//    {
+//        private int instanceCount = 0;
+//        private int staticCount = 0;
+
+//        public void increment()
+//        {
+//            instanceCount++;
+//            staticCount++;
+
+//            Console.WriteLine ("instance count: " + instanceCount);
+//            Console.WriteLine ("static count: " + staticCount);
+//        }
+//    }
+   
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            counter c1 = new counter();
+//            counter c2 = new counter();
+
+//            c1.increment();
+//            c1.increment();
+//            c2.increment();
+//            c2.increment(); 
+//        }
+//    }
+//}
---
+++ oops/methodswithnoparametersandnoreturntype.cs
@@ -0,0 +1,25 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace oops
+//{
+//    class myclass
+//    {
+//        public void Greet()
+//        {
+//            Console.WriteLine("hello!welcome to the methods");
+//        }
+//    }
+//    internal class methodswithnoparametersandnoreturntype
+//    {
+//        static void Main(string[] args)
+//        {
+//            myclass myclass = new myclass();
+//            myclass.Greet();
+//        }
+        
+//    }
+//}
