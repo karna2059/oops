8117b04faac8b3dd9a1c6a943577f3f9beb2c224
kkar2 <kkar2@LUFFY>
2026-05-12 12:30:12 +0530

    made changes

---
+++ oops/studentdetails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops
+{ 
+    class student
+    {
+       
+        public static void display()
+        {
+            Console.WriteLine("student id:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("student name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("student age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("student course:");
+            string course = Console.ReadLine();
+            
+           
+        }
+    }
+    internal class studentdetails
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 25; i++)
+            {
+                Console.WriteLine("Enter Details for Student " + i);
+                student.display();
+
+            }
+        }
+    }
+}
