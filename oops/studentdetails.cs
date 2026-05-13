0909de7a307055ef73e4839fde0b4f03fdddf5f5
kkar2 <kkar2@LUFFY>
2026-05-13 11:25:48 +0530

    made changes

--- oops/studentdetails.cs
+++ oops/studentdetails.cs
@@ -1,63 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace oops
-{
-    class student
-    {
-        int id;
-        string name;
-        int age;
-        string course;
+//namespace oops
+//{
+//    class student
+//    {
+//        int id;
+//        string name;
+//        int age;
+//        string course;
 
-        public void getdetails()
-        {
-            Console.WriteLine("student id:");
-             id = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("student name:");
-             name = Console.ReadLine();
-            Console.WriteLine("student age:");
-             age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("student course:");
-             course = Console.ReadLine();
+//        public void getdetails()
+//        {
+//            Console.WriteLine("student id:");
+//             id = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("student name:");
+//             name = Console.ReadLine();
+//            Console.WriteLine("student age:");
+//             age = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("student course:");
+//             course = Console.ReadLine();
             
 
 
-        }
-        public  void displaydetails()
-        {
-            Console.WriteLine("student details:");
-            Console.WriteLine("student id: " +id );
-            Console.WriteLine("student name: " + name);
-            Console.WriteLine("student age: " + age);
-            Console.WriteLine("student course: " + course);
-        }
+//        }
+//        public  void displaydetails()
+//        {
+//            Console.WriteLine("student details:");
+//            Console.WriteLine("student id: " +id );
+//            Console.WriteLine("student name: " + name);
+//            Console.WriteLine("student age: " + age);
+//            Console.WriteLine("student course: " + course);
+//        }
 
-    }
-    internal class studentdetails
-    {
-        static void Main(string[] args)
-        {
-            student student = new student();
+//    }
+//    internal class studentdetails
+//    {
+//        static void Main(string[] args)
+//        {
+//            student student = new student();
 
-            student[] students = new student[2];
-            for(int i = 0; i < students.Length; i++)
-            {
-                students[i] = new student();
-                students[i].getdetails();
+//            student[] students = new student[2];
+//            for(int i = 0; i < students.Length; i++)
+//            {
+//                students[i] = new student();
+//                students[i].getdetails();
                
-            }
-          for(int i = 0; i < students.Length; i++)
-            {
-                students[i].displaydetails();
-            }
+//            }
+//          for(int i = 0; i < students.Length; i++)
+//            {
+//                students[i].displaydetails();
+//            }
        
 
-        }
+//        }
 
 
-    }
-}
+//    }
+//}
---
+++ oops/studentmarks.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops
+{
+    internal class studentmarks
+    {
+        static void Main(string[] args)
+        {
+            int[] marks=new int[2];
+            string[] name=new string[2];
+            int[] age=new int[2];
+
+            for(int i=0;i<2;i++)
+            {
+                Console.WriteLine("enter marks");
+                marks[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter name");
+                name[i] = Console.ReadLine();
+                Console.WriteLine("enter age");
+                age[i]= Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("enter student name to search");
+            string search = Console.ReadLine();
+            for(int j=0;j<2;j++)
+            {
+                if (name[j] == search)
+                {
+                    Console.WriteLine("student name is :"+name[j]);
+                    Console.WriteLine("student marks is :"+marks[j]);
+                    Console.WriteLine("student age is :"+ age[j]);
+                }
+            }
+        }
+    }
+}
