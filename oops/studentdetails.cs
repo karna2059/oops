e3b046e5db68fc90f11527ea49646a336e880157
kkar2 <kkar2@LUFFY>
2026-05-13 10:59:05 +0530

    made changes

--- oops/studentdetails.cs
+++ oops/studentdetails.cs
@@ -5,35 +5,59 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace oops
-{ 
+{
     class student
     {
-       
-        public static void display()
+        int id;
+        string name;
+        int age;
+        string course;
+
+        public void getdetails()
         {
             Console.WriteLine("student id:");
-            int id = Convert.ToInt32(Console.ReadLine());
+             id = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("student name:");
-            string name = Console.ReadLine();
+             name = Console.ReadLine();
             Console.WriteLine("student age:");
-            int age = Convert.ToInt32(Console.ReadLine());
+             age = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("student course:");
-            string course = Console.ReadLine();
+             course = Console.ReadLine();
             
-           
+
+
         }
+        public  void displaydetails()
+        {
+            Console.WriteLine("student details:");
+            Console.WriteLine("student id: " +id );
+            Console.WriteLine("student name: " + name);
+            Console.WriteLine("student age: " + age);
+            Console.WriteLine("student course: " + course);
+        }
+
     }
     internal class studentdetails
     {
         static void Main(string[] args)
         {
+            student student = new student();
 
-            for (int i = 1; i <= 25; i++)
+            student[] students = new student[2];
+            for(int i = 0; i < students.Length; i++)
             {
-                Console.WriteLine("Enter Details for Student " + i);
-                student.display();
-
+                students[i] = new student();
+                students[i].getdetails();
+               
             }
+          for(int i = 0; i < students.Length; i++)
+            {
+                students[i].displaydetails();
+            }
+       
+
         }
+
+
     }
 }
