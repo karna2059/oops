c1be469eb3cf8ff468ae474b7b84c85485cacb0d
kkar2 <kkar2@LUFFY>
2026-05-14 12:36:20 +0530

    made changes

---
+++ oops/constructor.cs
@@ -0,0 +1,63 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace oops
+//{
+//    class myclass
+//    {
+//        int a;
+//        int b;
+
+//        public void display()
+//        {
+//            Console.WriteLine("value of a is :" + a);
+//            Console.WriteLine("value of b is :" + b);
+//        }
+//        public int add()
+//        {
+//            Console.WriteLine("addition is called");
+//            return a + b;
+//        }
+//        public int sub()
+//        {
+//            Console.WriteLine("subtraction is called");
+//            return a - b;
+//        }
+//        public int mul()
+//        {
+//            Console.WriteLine("multiplication is called");
+//            return a * b;
+
+//        }
+//        public int div()
+//        {
+//            Console.WriteLine("division is called");
+//            return a / b;
+//        }
+//        public myclass()
+//        {
+//            a = 10;
+//            b = 20;
+//            Console.WriteLine("constructor is called");
+//        }
+//    }
+//    internal class constructor
+//    {
+//        static void Main(string[] args)
+//        {
+//            myclass obj = new myclass();
+//            obj.display();
+//            int sum = obj.add();
+//            Console.WriteLine("sum is :" + sum);
+//            int sub = obj.sub();
+//            Console.WriteLine("sub is :" + sub);
+//            int mul = obj.mul();
+//            Console.WriteLine("mul is :" + mul);
+//            int div = obj.div();
+//            Console.WriteLine("div is :" + div);
+//        }
+//    }
+//}
---
+++ oops/empdetails.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops
+{
+    class emp
+    {
+        int empid;
+        string empname;
+        int empsalary;
+        public emp()
+        {
+            empid = 101;
+            empname = "karna";
+            empsalary = 50000;
+        }
+
+       
+        public void display()
+        {
+            Console.WriteLine("empid:" + empid);
+            Console.WriteLine("empname:" + empname);
+            Console.WriteLine("empsalary:" + empsalary);
+            Console.WriteLine("salary hike is :"+ (empsalary * 0.1));
+        }
+
+        public int totalSalary()
+        {
+            return empsalary + (int)(empsalary * 0.1);
+        }
+
+
+
+        public static void displaycompany()
+        {
+            Console.WriteLine("company name is: wipro");
+        }
+
+    }
+    internal class empdetails
+    {
+        static void Main(string[] args)
+        {
+            emp emps=new emp();
+          
+            emps.display();
+           
+            emps.totalSalary();
+            Console.WriteLine("total salary is:" + emps.totalSalary());
+            emp.displaycompany();
+        }
+    }
+}
