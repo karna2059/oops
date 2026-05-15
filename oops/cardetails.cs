61d50532eb5c27d934bd2c29a6f8d2ea02bb67ba
kkar2 <kkar2@LUFFY>
2026-05-15 11:51:00 +0530

    made changes

---
+++ oops/cardetails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops
+{
+    class car
+    {
+        string ModelOfCar;  
+        string ColorOfCar;
+        int yearofManufacture;
+
+        public car(string model, string color, int year)
+        {
+            ModelOfCar = model;
+            ColorOfCar = color;
+            yearofManufacture = year;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("model of car: " + ModelOfCar);
+            Console.WriteLine("color of car: " + ColorOfCar);
+            Console.WriteLine("year of manufacture: " + yearofManufacture);
+        }
+    }
+    internal class cardetails
+    {
+        static void Main(string[] args)
+        {
+            car car1 = new car("BMW", "black", 2020);
+            car1.display();
+             car car2 = new car("Audi", "white", 2019);
+            car2.display();
+        }
+    }
+}
--- oops/sqlcon.cs
+++ oops/sqlcon.cs
@@ -1,56 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Data.SqlClient;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace oops
-{
-    class DataBaseConnection
-    {
-        string ConnectionString;
-        SqlConnection connection;
+//namespace oops
+//{
+//    class DataBaseConnection
+//    {
+//        string ConnectionString;
+//        SqlConnection connection;
 
-        public DataBaseConnection(string connstring)
-        {
-            ConnectionString=connstring;
-            connection=new SqlConnection(ConnectionString);
-        }
+//        public DataBaseConnection(string connstring)
+//        {
+//            ConnectionString=connstring;
+//            connection=new SqlConnection(ConnectionString);
+//        }
 
-        public void OpenConnection()
-        {
-            try
-            {
-                connection.Open();
-                Console.WriteLine("Connection opened successfully.");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error opening connection: " + e.Message);
-            }
+//        public void OpenConnection()
+//        {
+//            try
+//            {
+//                connection.Open();
+//                Console.WriteLine("Connection opened successfully.");
+//            }
+//            catch (Exception e)
+//            {
+//                Console.WriteLine("Error opening connection: " + e.Message);
+//            }
 
             
-        }
+//        }
 
-        public void CloseConnection()
-        {
-            if(connection.State == System.Data.ConnectionState.Open)
-            {
-                connection.Close();
-                Console.WriteLine("Connection closed successfully.");
-            }
-        }
-    }
+//        public void CloseConnection()
+//        {
+//            if(connection.State == System.Data.ConnectionState.Open)
+//            {
+//                connection.Close();
+//                Console.WriteLine("Connection closed successfully.");
+//            }
+//        }
+//    }
 
-    internal class sqlcon
-    {
-        static void Main(string[] args)
-        {
-            string connstring = "Data Source=Luffy\\SQLEXPRESS;Initial Catalog=RegisterForm;Integrated Security=True";
-            DataBaseConnection db = new DataBaseConnection(connstring);
-            db.OpenConnection();
-            db.CloseConnection();
-        }
-    }
-}
+//    internal class sqlcon
+//    {
+//        static void Main(string[] args)
+//        {
+//            string connstring = "Data Source=Luffy\\SQLEXPRESS;Initial Catalog=RegisterForm;'[Integrated Security=True";
+//            DataBaseConnection db = new DataBaseConnection(connstring);
+//            db.OpenConnection();
+//            db.CloseConnection();
+//        }
+//    }
+//}
