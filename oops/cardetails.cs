f26c87fc3bd8e2f8a01296ead1b784c23c57b029
kkar2 <kkar2@LUFFY>
2026-05-15 12:40:34 +0530

    made changes

--- oops/cardetails.cs
+++ oops/cardetails.cs
@@ -6,34 +6,55 @@ using System.Threading.Tasks;
 
 namespace oops
 {
-    class car
-    {
-        string ModelOfCar;  
-        string ColorOfCar;
-        int yearofManufacture;
+    using System;
 
-        public car(string model, string color, int year)
+    namespace oops
+    {
+        class car
         {
-            ModelOfCar = model;
-            ColorOfCar = color;
-            yearofManufacture = year;
+            string ModelOfCar;
+            string ColorOfCar;
+            int yearofManufacture;
+
+            public car(string model, string color, int year)
+            {
+                ModelOfCar = model;
+                ColorOfCar = color;
+                yearofManufacture = year;
+            }
+            public void display()
+            {
+                Console.WriteLine("Model of car: " + ModelOfCar);
+                Console.WriteLine("Color of car: " + ColorOfCar);
+
+                if (yearofManufacture < 1886)
+                {
+
+                    Console.WriteLine("Invalid year of manufacture");
+
+                }
+                else
+                {
+                    Console.WriteLine("Year of manufacture: " + yearofManufacture);
+
+                }
+
+
+            }
         }
 
-        public void display()
+        internal class cardetails
         {
-            Console.WriteLine("model of car: " + ModelOfCar);
-            Console.WriteLine("color of car: " + ColorOfCar);
-            Console.WriteLine("year of manufacture: " + yearofManufacture);
-        }
-    }
-    internal class cardetails
-    {
-        static void Main(string[] args)
-        {
-            car car1 = new car("BMW", "black", 2020);
-            car1.display();
-             car car2 = new car("Audi", "white", 2019);
-            car2.display();
+            static void Main(string[] args)
+            {
+                car car1 = new car("BMW", "Black", 2020);
+                car1.display();
+
+                car car2 = new car("Audi", "White", 1884);
+                car2.display();
+
+            
+            }
         }
     }
 }
