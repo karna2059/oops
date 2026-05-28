2f42ff7955c85ce329a7b0644d0a6c62ea5fa671
kkar2 <kkar2@LUFFY>
2026-05-28 18:20:06 +0530

    made changes

--- oops/abstractclass.cs
+++ oops/abstractclass.cs
@@ -1,108 +1,122 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace oops
-{
-
-
-
-    abstract class Person
-    {
-
-        static Person()
-        {
-            Console.WriteLine("School Management Details");
-        }
-
-          public abstract void schoolmanagement();
-
-        public void Schoolname()
-        {
-
-            Console.WriteLine("School Name :st.Joseph");
-        }
-    }
-
-    class Teacher : Person
-    {
-       public string name {  get; set; }
-      public string subject {  get; set; }
-
-        public Teacher(string name, string subject)
-        {
-            this.name = name;
-            this.subject = subject;
-        }
-
-        public override void schoolmanagement()
-        {
-            Console.WriteLine("-----Teacher Deatils -----");
-            Console.WriteLine("Teacher Name : " + name);
-            Console.WriteLine("Subject      : " + subject);
-        }
-    }
-
-    class Student : Person
-    {
-       public int rollno {  get; set; }
-       public string name {  get; set; }
-        public int age {  get; set; }
+//namespace oops
+//{
 
 
-        public Student(int rollno, string name, int age)
-        {
-            this.rollno = rollno;
-            this.name = name;
-            this.age = age;
-        }
 
-        public override void schoolmanagement()
-        {
-            Console.WriteLine("-----Studnet Details -----");
-            Console.WriteLine("Student Rollno : " + rollno);
-            Console.WriteLine("Student Name   : " + name);
-            Console.WriteLine("Student Age    : " + age);
-        }
-    }
+//    abstract class Person
+//    {
+//        public string location;
 
-    class Program
-    {
-        static void Main(string[]args)
-        {
+//        public Person(string location)
+//        {
+//            this.location = location;
+//            Console.WriteLine("school location is "+location);
+//        }
+
+//        static Person()
+//        {
+//            Console.WriteLine("School Management Details");
+//        }
+
+//          public abstract void schoolmanagement();
+
+//        public void Schoolname()
+//        {
+
+//            Console.WriteLine("School Name :st.Joseph");
+//        }
+//    }
+
+//    class Teacher : Person
+//    {
+//       public string name {  get; set; }
+//      public string subject {  get; set; }
+
+//        public Teacher(string location,string name, string subject) 
+//            : base(location)
+//        {
+//            this.name = name;
+//            this.subject = subject;
+//        }
+
+//        public override void schoolmanagement()
+//        {
+//            Console.WriteLine("-----Teacher Deatils -----");
+//            Console.WriteLine("Teacher Name : " + name);
+//            Console.WriteLine("Subject      : " + subject);
+//        }
+//    }
+
+//    class Student : Person
+//    {
+//       public int rollno {  get; set; }
+//       public string name {  get; set; }
+//        public int age {  get; set; }
+
+
+//        public Student(string location, int rollno, string name, int age) 
+//            : base(location)
+//        {
+//            this.rollno = rollno;
+//            this.name = name;
+//            this.age = age;
+//        }
+
+//        public override void schoolmanagement()
+//        {
+//            Console.WriteLine("-----Studnet Details -----");
+//            Console.WriteLine("Student Rollno : " + rollno);
+//            Console.WriteLine("Student Name   : " + name);
+//            Console.WriteLine("Student Age    : " + age);
+//        }
+//    }
+
+//    class Program
+//    {
+//        static void Main(string[]args)
+//        {
        
-            Student s = new Student(101,"karna",22);
+//            Student s = new Student("hyderabad",101,"karna",22);
 
-            Teacher t = new Teacher("sai kumar", ".Net Fullstack");
+//            Teacher t = new Teacher("chennai","sai kumar", ".Net Fullstack");
 
-            s.Schoolname();
+//            s.Schoolname();
 
-            t.schoolmanagement();
+//            t.schoolmanagement();
 
-            s.schoolmanagement();
+//            s.schoolmanagement();
 
-            s.rollno= 102;
+//            s.rollno= 102;
 
-            s.name = "shiva";
+//            s.name = "shiva";
 
-            s.age = 21;
+//            s.age = 21;
 
-            Console.WriteLine("student name is : "+ s.rollno);
-            Console.WriteLine("student name is :" + s.name);
-            Console.WriteLine("student age is  :" + s.age);
-
-
-            t.name = "santhosh";
-
-            t.subject = "java";
-
-            Console.WriteLine("teacher name is :" + t.name);
-            Console.WriteLine("subject is :" + t.subject);
+//            Console.WriteLine("student name is : "+ s.rollno);
+//            Console.WriteLine("student name is :" + s.name);
+//            Console.WriteLine("student age is  :" + s.age);
 
 
+//            t.name = "santhosh";
 
-        }
-    }
-}
+//            t.subject = "java";
+
+//            Console.WriteLine("teacher name is :" + t.name);
+//            Console.WriteLine("subject is :" + t.subject);
+
+
+
+//        }
+//    }
+//}
+
+
+
+
+
---
+++ oops/sms.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops
+{
+    using System;
+
+    abstract class Person
+    {
+        // Properties
+        public string name { get; set; }
+        public int age { get; set; }
+
+        // Constructor
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+
+            Console.WriteLine("Person Constructor is Called");
+        }
+
+        // Normal Method
+        public void displayPerson()
+        {
+            Console.WriteLine("Person Name : " + name);
+            Console.WriteLine("Person Age  : " + age);
+        }
+
+        // Abstract Method
+        public abstract void Role(string sub);
+    }
+
+    // Student Class
+    class Student : Person
+    {
+        public int RollNo { get; set; }
+
+        // Constructor
+        public Student(int RollNo)
+            : base("Sai", 25)
+        {
+            this.RollNo = RollNo;
+        }
+
+        // Override Method
+        public override void Role(string sub)
+        {
+            Console.WriteLine("Student Subject is : " + sub);
+        }
+    }
+
+    // Teacher Class
+    class Teacher : Person
+    {
+        public string subject { get; set; }
+
+        // Constructor
+        public Teacher(string subject)
+            : base("Ram", 40)
+        {
+            this.subject = subject;
+        }
+
+        // Override Method
+        public override void Role(string sub)
+        {
+            Console.WriteLine("Teacher Role is to teach : " + sub);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Student Object
+            Student s = new Student(101);
+
+            s.displayPerson();
+
+            Console.WriteLine("Student RollNo : " + s.RollNo);
+
+            s.Role(".Net");
+
+            Console.WriteLine();
+
+            // Teacher Object
+            Teacher t = new Teacher("C#");
+
+            t.displayPerson();
+
+            Console.WriteLine("Teacher Subject : " + t.subject);
+
+            t.Role("C#");
+        }
+    }
+}
