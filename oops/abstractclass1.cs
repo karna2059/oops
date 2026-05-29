6690268a749c65feb099213bb2ed714e2a2ef819
kkar2 <kkar2@LUFFY>
2026-05-29 15:34:49 +0530

    made changes

--- oops/abstractclass1.cs
+++ oops/abstractclass1.cs
@@ -1,68 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace oops
-{
-    abstract class person
-    {
-        protected string name;
-        private int age;
+//namespace oops
+//{
+//    abstract class person
+//    {
+//        protected string name;
+//        private int age;
 
-        public person(string name, int age)
-        {
-            this.name = name;
-            this.age = age;
-            Console.WriteLine("person constructor called");
-        }
-        public void display()
-        {
-            Console.WriteLine("student name :"+name);
-            Console.WriteLine("student age is :" + age);
-        }
-        public abstract void role();
+//        public person(string name, int age)
+//        {
+//            this.name = name;
+//            this.age = age;
+//            Console.WriteLine("person constructor called");
+//        }
+//        public void display()
+//        {
+//            Console.WriteLine("student name :"+name);
+//            Console.WriteLine("student age is :" + age);
+//        }
+//        public abstract void role();
        
-    }
+//    }
 
-    class student : person
-    {
-        public int rollno;
+//    class student : person
+//    {
+//        public int rollno;
 
-        public student(string name,int age, int rollno) : base(name,age)
-        {
-            this.rollno = rollno;
-        }
-        public override void role()
-        {
-            Console.WriteLine("roll no is :" + rollno);
-            Console.WriteLine("I am a student ");
-        }
-    }
+//        public student(string name,int age, int rollno) : base(name,age)
+//        {
+//            this.rollno = rollno;
+//        }
+//        public override void role()
+//        {
+//            Console.WriteLine("roll no is :" + rollno);
+//            Console.WriteLine("I am a student ");
+//        }
+//    }
 
-    class teacher : person
-    {
-        public teacher(string name, int age ) : base(name, age)
-        {
-
-        }
-        public override void role()
-        {
-            Console.WriteLine("I am a teacher");
-        }
-    }
-    internal class abstractclass1
-    {
-        static void Main(string[] args)
-        {
-            person s = new student("karna",22,101);
-            s.display();
-            s.role();
-            person t = new teacher("sai kumar", 40);
-            t.display();
-            t.role();
+//    class teacher : person
+//    {
+//        public string subject;
+//        public teacher(string name, int age ,string subject) : base(name, age)
+//        {
+//            this.subject = subject;
+//        }
+//        public override void role()
+//        {
+//            Console.WriteLine("teacher subject is :"+subject);
+//            Console.WriteLine("I am a teacher");
+//        }
+//    }
+//    internal class abstractclass1
+//    {
+//        static void Main(string[] args)
+//        {
+//            person s = new student("karna",22,101);
+//            s.display();
+//            s.role();
+//            person t = new teacher("sai kumar", 40,".Net Fullstack");
+//            t.display();
+//            t.role();
             
-        }
-    }
-}
+//        }
+//    }
+//}
---
+++ oops/abstractsingleinheritence.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops
+{
+    abstract class vehicle
+    {
+        protected string vehiclename;
+
+        public vehicle(string vehiclename)
+        {
+            this.vehiclename = vehiclename;
+            Console.WriteLine("vehicle is called");
+        }
+        public void display()
+        {
+            Console.WriteLine($"vehicle :{vehiclename}");
+        }
+        public abstract void vehicletype();
+    }
+
+    abstract class vehiclemodel : vehicle
+    {
+        protected int year;
+        public vehiclemodel(string vehiclename ,int year) : base(vehiclename)
+        {
+            this.year = year;
+            Console.WriteLine("vehicle model is called");
+        }
+        public void displayinfo()
+        {
+            Console.WriteLine($"vehicle year :{year}");
+        }
+        public abstract void modeltype();
+    }
+
+    class vehicledata : vehiclemodel
+    {
+
+        protected string colour;
+        public vehicledata(string colour, string vehiclename,int year) : base(vehiclename, year)
+        {
+           this.colour = colour;
+            Console.WriteLine("vehicledata is called");
+        }
+        public void disp()
+        {
+            Console.WriteLine("car colour is :" + colour);
+        }
+        public override void vehicletype()
+        {
+            Console.WriteLine("car");
+        }
+        public override void modeltype()
+        {
+            Console.WriteLine("electric");
+        }
+
+
+
+
+    }
+
+    internal class abstractsingleinheritence
+    {
+        static void Main()
+        {
+            vehicledata v = new vehicledata("Black","Tesla", 2025);
+
+            v.display();
+            v.displayinfo();
+            v.disp();
+            v.vehicletype();
+            v.modeltype();
+        }
+    }
+}
