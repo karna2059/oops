acd5965a097a5e908c112cad0fd35d9b96279e07
kkar2 <kkar2@LUFFY>
2026-05-29 11:18:07 +0530

    made changes

---
+++ oops/abstractclass1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops
+{
+    abstract class person
+    {
+        protected string name;
+        private int age;
+
+        public person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+            Console.WriteLine("person constructor called");
+        }
+        public void display()
+        {
+            Console.WriteLine("student name :"+name);
+            Console.WriteLine("student age is :" + age);
+        }
+        public abstract void role();
+       
+    }
+
+    class student : person
+    {
+        public int rollno;
+
+        public student(string name,int age, int rollno) : base(name,age)
+        {
+            this.rollno = rollno;
+        }
+        public override void role()
+        {
+            Console.WriteLine("roll no is :" + rollno);
+            Console.WriteLine("i am a student ");
+        }
+    }
+    internal class abstractclass1
+    {
+        static void Main(string[] args)
+        {
+            student s = new student("karna",22,101);
+            s.display();
+            s.role();
+            
+        }
+    }
+}
--- oops/sms.cs
+++ oops/sms.cs
@@ -1,100 +1,100 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace oops
-{
-    using System;
+//namespace oops
+//{
+//    using System;
 
-    abstract class Person
-    {
-        // Properties
-        public string name { get; set; }
-        public int age { get; set; }
+//    abstract class Person
+//    {
+//        // Properties
+//        public string name { get; set; }
+//        public int age { get; set; }
 
-        // Constructor
-        public Person(string name, int age)
-        {
-            this.name = name;
-            this.age = age;
+//        // Constructor
+//        public Person(string name, int age)
+//        {
+//            this.name = name;
+//            this.age = age;
 
-            Console.WriteLine("Person Constructor is Called");
-        }
+//            Console.WriteLine("Person Constructor is Called");
+//        }
 
-        // Normal Method
-        public void displayPerson()
-        {
-            Console.WriteLine("Person Name : " + name);
-            Console.WriteLine("Person Age  : " + age);
-        }
+//        // Normal Method
+//        public void displayPerson()
+//        {
+//            Console.WriteLine("Person Name : " + name);
+//            Console.WriteLine("Person Age  : " + age);
+//        }
 
-        // Abstract Method
-        public abstract void Role(string sub);
-    }
+//        // Abstract Method
+//        public abstract void Role(string sub);
+//    }
 
-    // Student Class
-    class Student : Person
-    {
-        public int RollNo { get; set; }
+//    // Student Class
+//    class Student : Person
+//    {
+//        public int RollNo { get; set; }
 
-        // Constructor
-        public Student(int RollNo)
-            : base("Sai", 25)
-        {
-            this.RollNo = RollNo;
-        }
+//        // Constructor
+//        public Student(int RollNo)
+//            : base("Sai", 25)
+//        {
+//            this.RollNo = RollNo;
+//        }
 
-        // Override Method
-        public override void Role(string sub)
-        {
-            Console.WriteLine("Student Subject is : " + sub);
-        }
-    }
+//        // Override Method
+//        public override void Role(string sub)
+//        {
+//            Console.WriteLine("Student Subject is : " + sub);
+//        }
+//    }
 
-    // Teacher Class
-    class Teacher : Person
-    {
-        public string subject { get; set; }
+//    // Teacher Class
+//    class Teacher : Person
+//    {
+//        public string subject { get; set; }
 
-        // Constructor
-        public Teacher(string subject)
-            : base("Ram", 40)
-        {
-            this.subject = subject;
-        }
+//        // Constructor
+//        public Teacher(string subject)
+//            : base("Ram", 40)
+//        {
+//            this.subject = subject;
+//        }
 
-        // Override Method
-        public override void Role(string sub)
-        {
-            Console.WriteLine("Teacher Role is to teach : " + sub);
-        }
-    }
+//        // Override Method
+//        public override void Role(string sub)
+//        {
+//            Console.WriteLine("Teacher Role is to teach : " + sub);
+//        }
+//    }
 
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // Student Object
-            Student s = new Student(101);
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // Student Object
+//            Student s = new Student(101);
 
-            s.displayPerson();
+//            s.displayPerson();
 
-            Console.WriteLine("Student RollNo : " + s.RollNo);
+//            Console.WriteLine("Student RollNo : " + s.RollNo);
 
-            s.Role(".Net");
+//            s.Role(".Net");
 
-            Console.WriteLine();
+//            Console.WriteLine();
 
-            // Teacher Object
-            Teacher t = new Teacher("C#");
+//            // Teacher Object
+//            Teacher t = new Teacher("C#");
 
-            t.displayPerson();
+//            t.displayPerson();
 
-            Console.WriteLine("Teacher Subject : " + t.subject);
+//            Console.WriteLine("Teacher Subject : " + t.subject);
 
-            t.Role("C#");
-        }
-    }
-}
+//            t.Role("C#");
+//        }
+//    }
+//}
